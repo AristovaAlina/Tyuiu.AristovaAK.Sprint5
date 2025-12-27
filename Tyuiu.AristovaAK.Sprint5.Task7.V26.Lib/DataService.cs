8848fed5a589073becd5a0d91a40a108bb9e7015
AristovaAlina <alinaaristova72@gmail.com>
2025-12-27 08:09:14 +0500

    Task7

---
+++ Tyuiu.AristovaAK.Sprint5.Task7.V26.Lib/DataService.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AristovaAK.Sprint5.Task7.V26.Lib
+{
+    public class DataService : ISprint5Task7V26
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string savePath = Path.Combine(@"C:\DataSprint5", "OutPutDataFileTask7V26.txt");
+
+            FileInfo fileInfo = new FileInfo(savePath);
+            bool exists = fileInfo.Exists;
+            if (exists)
+            {
+                File.Delete(savePath);
+            }
+
+            string text = File.ReadAllText(path);
+
+            string result = "";
+            string currentWord = "";
+
+            foreach (char c in text)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    currentWord += c;
+                }
+                else
+                {
+                    if (currentWord.Length > 0)
+                    {
+                        result += "word"; 
+                        currentWord = "";
+                    }
+                    result += c;
+                }
+            }
+
+            if (currentWord.Length > 0)
+            {
+                result += "word";
+            }
+
+            File.WriteAllText(savePath, result);
+            return savePath;
+        }
+    }
+}
---
+++ Tyuiu.AristovaAK.Sprint5.Task7.V26/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.AristovaAK.Sprint5.Task7.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Аристова А. К. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан файл, в котором есть набор символьных данных.Заменить все           *");
+        Console.WriteLine("* английские слова на слово 'word'.Полученный результат сохранить в       *");
+        Console.WriteLine("* файл OutPutDataFileTask7V26.txt.                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask7V26.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        string tempPath = Path.GetTempPath();
+        string inputFilePath = Path.Combine(@"C:\DataSprint5", "InPutDataFileTask7V26.txt");
+        string outputFilePath = Path.Combine(tempPath, "OutPutDataFileTask7V26.txt");
+
+        string res = ds.LoadDataAndSave(path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
